4c1706efa18799eb0dca893cba92e31d48fdc2c3
Karan Rampal <karan.rampal@hm.com>
2022-01-21 16:54:24 +0100

    Add visualization to notebook

--- src/utils/vis_utils.py
+++ src/utils/vis_utils.py
@@ -8,31 +8,47 @@ import matplotlib.patches as patches
 import numpy as np
 
 
-def vis_data(img: np.ndarray, target: Dict[str, Any], cats: Dict[int, str], score: bool = False, num_cols: int = 3, figsize: Tuple[int, int] = (20, 30)) -> None:
-    """Visualze the data"""
+def vis_data(img: np.ndarray,
+             target: Dict[str, Any],
+             categories: Dict[int, str],
+             num_cols: int = 3,
+             figsize: Tuple[int, int] = (20, 30)) -> None:
+    """Visualze the data
+    Args:
+        img: Input image
+        target: Dictionary of ground truth labels or predictions
+        num_cols: Number of columns in the visuazilation grid
+        figsize: Figure size
+    """
     boxes = target["boxes"].detach().numpy()
     labels = target["labels"].detach().numpy()
     masks = target["masks"].detach().numpy()
-    if score:
+    if "scores" in target:
         scores = target["scores"].detach().numpy()
-    num = len(boxes)
+    num = len(boxes) + 1
     num_rows = math.ceil(num / num_cols)
     _, ax = plt.subplots(num_rows, num_cols, figsize=figsize)
     ax = ax.flatten()
 
-    for i in range(num):
-        ax[i].imshow(img)
-        ax[i].imshow(masks[i].squeeze(), alpha=0.7)
+    ax[0].imshow(img)
+    ax[0].set_title(f"Input image")
+    ax[0].axis("off")
+    for i in range(1, num):
+        j = i - 1
 
-        label = labels[i]
-        title = f"Label: {label} ({cats[label - 1]})"
-        if score:
-            title += f" Score: {scores[i]}"
+        ax[i].imshow(img)
+        ax[i].imshow(masks[j].squeeze(), alpha=0.7)
+
+        label = labels[j]
+        title = f"Label: {label} ({categories[label - 1]})"
+        if "scores" in target:
+            title += f" Score: {scores[j]}"
         ax[i].set_title(title)
 
-        x1, y1, x2, y2 = boxes[i]
+        x1, y1, x2, y2 = boxes[j]
         rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=1, edgecolor='r', facecolor='none')
         ax[i].add_patch(rect)
+        ax[i].axis("off")
 
     for axi in ax[i:]:
         axi.axis("off")
